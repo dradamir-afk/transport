32e151a1878c2ca52b901c4c3e1ae4566e7f3963
Дамир Мухомедзянов <G38q@yandex.ru>
2020-07-01 00:57:01 +0300

    First Page

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AuthoPage></AuthoPage>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AuthoPage from './components/AuthoPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AuthoPage
   }
 }
 </script>
 
 <style>
+body{
+  width: 100%;
+  height:100%;
+  margin:auto;
+  background-color: #edf1fd;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/AuthoPage.vue
@@ -0,0 +1,119 @@
+<template>
+    <div class="container">
+        <h1 class="welcome">
+        Welcome to your <br/> Logistic Capasity Optimization Tool
+        </h1>
+        <form>
+            <div class="area">
+                <div class="autho">
+                    <input type="text" class="username" placeholder="Your ABI Username">
+                    <input type="text" class="password" placeholder="Your ABI Password">
+                </div>
+                    <div class="submitcont">
+                    <input type="submit" class="submit" value="Login">
+                </div>
+            </div>
+        </form>
+        <button class="support">Support</button>
+        <div class="logo">
+                <img src="https://img.icons8.com/ios/50/000000/cute-termite.png"/>
+        </div>
+        <div class="poweredby">
+            <img src="https://img.icons8.com/ios/50/000000/ant.png"/>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@100&display=swap');
+.container{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    
+}
+
+.welcome {
+    font-family: 'Raleway', sans-serif;
+    font-size: 44px;
+    text-align: center;
+    margin-top: 200px;
+}
+.area{
+    display: flex;
+    flex-direction: column;
+    width: 580px;
+    height: 150px;
+    margin-top: 50px;
+}
+.autho{
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+}
+.username{
+    font-size: 15px;
+    height: 60px;
+    width: 345px;
+    margin-right: 10px;
+    border: none;
+}
+.password{
+    font-size: 15px;
+    height: 60px;
+    width: 345px;
+    border: none;
+}
+.submitcont{
+    height: 60px;
+    width: 580px;
+    margin-top: 20px;
+    display: flex;
+    justify-content: center;
+}
+.submit{
+    font-size: 15px;
+    width: 100%;
+    height: 100%;
+    border: none;
+    background-color: #5198fc;
+    border-radius: 15px;
+    color: white;
+}
+
+.support{
+    width: 150px;
+    height: 35px;
+    margin-top: 30px;
+    background-color: #5a81fb;
+    border: none;
+    border-radius: 15px;
+    color:white;
+    font-size: 15px;
+}
+
+.logo{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 50px;
+}
+
+.poweredby{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 150px;
+}
+</style>
